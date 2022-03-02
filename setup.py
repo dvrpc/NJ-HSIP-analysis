360d53b302be15cc1b873f267cd4b677b007a4b5
Aaron Fraint <38364429+aaronfraint@users.noreply.github.com>
2022-03-02 12:41:34 -0500

    add Python environment and package

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import find_packages, setup
+
+setup(
+    name="nj_hsip_analyis",
+    packages=find_packages(),
+    version="0.0.1",
+    description="Spatial analysis of NJ's Highway Safety Improvement Program data",
+    author="Aaron Fraint, AICP",
+    license="GPL-3.0",
+)
